fdb1e94c6906c69a9b75ea418516b3e36409b625
AAM1337 <amacinov@gmail.com>
2025-11-05 16:48:38 +0700

    Рабочий опросник без дизайна

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -45,8 +50,60 @@ fun Greeting(innerPadding: PaddingValues) {
     var currentIndex by remember { mutableIntStateOf(0) }
     var correctAnswers by remember { mutableIntStateOf(0) }
     var answered by remember { mutableStateOf(false) }
-}
 
+    Column(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+        if (currentIndex < questions.size) {
+            QuestionCard(
+                question = questions[currentIndex],
+                onAnswer = { userAnswer ->
+                    if (!answered) {
+                        if (userAnswer == questions[currentIndex].answer) {
+                            correctAnswers++
+                        }
+                        answered = true
+                    }
+                },
+                buttonsEnabled = !answered
+            )
+            if (answered) {
+                Button(
+                    onClick = {
+                        if (currentIndex < questions.lastIndex) {
+                            currentIndex++
+                            answered = false
+                        }
+                    },
+                    enabled = currentIndex < questions.lastIndex
+                ) {
+                    Text("Next")
+                }
+            }
+        }
+        if (currentIndex == questions.lastIndex && answered) {
+            Text("Вы набрали $correctAnswers из ${questions.size}")
+        }
+    }
+}
+@Composable
+fun QuestionCard(
+    question: Question,
+    onAnswer: (Boolean) -> Unit,
+    buttonsEnabled: Boolean
+) {
+    Column {
+        Text(text = question.text)
+        if (buttonsEnabled) {
+            Row {
+                Button(onClick = { onAnswer(true) }) {
+                    Text("True")
+                }
+                Button(onClick = { onAnswer(false) }) {
+                    Text("False")
+                }
+            }
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
